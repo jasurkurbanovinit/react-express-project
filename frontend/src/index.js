2cc286567b70167dc5e608f22d6a6a6a0f2a7c91
jasurkurbanovinit <jasurkurbanov96@gmail.com>
2020-12-12 09:49:13 +0500

    front and backend added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import store from './store';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, document.getElementById('root'));
-
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
